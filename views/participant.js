9a1a56a743c3d63a1ad0e257ac90266adab98339
root <root@localhost.localdomain>
2018-01-30 00:54:39 +0530

    removedsomefiles

--- views/participant.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('myValidateApp', []);
-app.controller('validateCtrl', function($scope) {
-
-   $scope.submitForm = function(isValid) {
-   if (isValid) {
-     alert('form has been submitted and the participant has been enrolled');
-   }
-};
-});
